c7fd49634cbfd770659104aac1571c384a4d8dab
Nikhil Pawar <nikhil0909pawar@gmail.com>
2026-06-09 19:16:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.7%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countDigitOne(int n) {
+        int count = 0;
+        for(long i = 1; i <= n; i *= 10){
+            long divider = i * 10;
+            count += (n / divider) * i;
+            count += Math.min(Math.max(n % divider - i + 1, 0), i);
+        }
+        return count;
+    }
+}
